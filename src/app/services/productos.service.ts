0a6db53db0399ca7853110a11dbf5c024fe1b7c0
Jose Arizapana <arizapana@outlook.com>
2020-04-10 23:58:17 -0500

    Fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { registerLocaleData } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +10,50 @@ export class ProductosService {
 
   cargando=true;
   productos: Producto[]=[];
-
+  productosFiltrado: Producto[]=[];
   constructor(private http: HttpClient) { 
 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-e27db.firebaseio.com/productos_idx.json')
-            .subscribe((resp:Producto[])=>{
-      console.log(resp);
-      this.productos=resp;
-      this.cargando = false;
-    }); 
+
+    return new Promise((resolve,reject)=>{
+      this.http.get('https://angular-html-e27db.firebaseio.com/productos_idx.json')
+      .subscribe((resp:Producto[])=>{
+        this.productos=resp;
+        this.cargando = false;
+        resolve();
+      }); 
+    });
+  }
+  getProducto(id:String){
+    return this.http.get(`https://angular-html-e27db.firebaseio.com/productos/${id}.json`)
+  }
+  buscarProducto(termino: string){
+
+    if(this.productos.length ===0){
+      this.cargarProductos().then(()=>{
+        this.filtrarProductos(termino);
+      });
+    }else{
+      this.filtrarProductos(termino);
+    }
+  }
+  
+  private filtrarProductos(termino:string){
+
+    this.productosFiltrado=[];
+    termino=termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod=>{
+
+      const tituloLower= prod.titulo.toLocaleLowerCase();
+      if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0){
+        this.productosFiltrado.push(prod);
+      }
+
+    });
   }
 
 }
